2988067cccf72131a2af5bcda5a8e053fde814b5
Joe G <leonisminis@gmail.com>
2022-10-07 15:12:19 -0400

    Attempt to fix js via freestuff.dev implementation.

--- assets/js/active_search.js
+++ assets/js/active_search.js
@@ -19,11 +19,11 @@ function liveSearch() {
 }
 
 //A little delay
-let typingTimer;
-let typeInterval = 500;
-let searchInput = document.getElementById('searchbox');
+//let typingTimer;
+//let typeInterval = 500;
+//let searchInput = document.getElementById('searchbox');
 
-searchInput.addEventListener('keyup', () => {
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(liveSearch, typeInterval);
-});
+//searchInput.addEventListener('keyup', () => {
+//    clearTimeout(typingTimer);
+//    typingTimer = setTimeout(liveSearch, typeInterval);
+//});
