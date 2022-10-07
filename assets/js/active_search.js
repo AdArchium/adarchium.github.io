d4b34c87b15c2f1749884aeae1cf08f641d5a7ab
Joe G <leonisminis@gmail.com>
2022-10-07 15:17:40 -0400

    Syntax fix to search js.

--- assets/js/active_search.js
+++ assets/js/active_search.js
@@ -9,7 +9,7 @@ function liveSearch() {
     //Use innerText if all contents are visible
     //Use textContent for including hidden elements
     for (var i = 0; i < cards.length; i++) {
-        if(cards[i].innerContent.toLowerCase()
+        if(cards[i].innerText.toLowerCase()
                 .includes(search_query.toLowerCase())) {
             cards[i].classList.remove("is-hidden");
         } else {
