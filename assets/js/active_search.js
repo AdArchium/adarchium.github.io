5b6c23c8ee97bfb6a735df31fc1eb0c79855bc31
Joe G <leonisminis@gmail.com>
2022-12-23 20:07:38 -0500

    See if switching to window.onload will help.

--- assets/js/active_search.js
+++ assets/js/active_search.js
@@ -1,13 +1,15 @@
-let timer;
-const input = document.querySelector("#searchbox");
-input.addEventListener("keyup", function (liveSearch) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        const items = document.querySelectorAll(".cards");
-        for (let item of items) {
-            item.style.display = item.textContent.includes(liveSearch.target.value)
-            ? "inline-block"
-            : "none";
-        }
-    }, 1000);
-});
+window.onload=function(){
+    let timer;
+    const input = document.querySelector("#searchbox");
+    input.addEventListener("keyup", function (liveSearch) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            const items = document.querySelectorAll(".cards");
+            for (let item of items) {
+                item.style.display = item.textContent.includes(liveSearch.target.value)
+                ? "inline-block"
+                : "none";
+            }
+        }, 1000);
+    });
+}
